04bcad410321220eee6d5c3ce89e7433d3df036d
unknown <Raphaelspraul@hotmail.de>
2017-11-25 15:42:03 +0100

    Singleton-Pattern implementiert.

--- SysInfo.h
+++ SysInfo.h
@@ -6,13 +6,22 @@ class SysInfo {
 
 public:
 
-    SysInfo();
+    static SysInfo& instance();             //Singleton: "unique access point", nur hierüber kommt man an das Objekt
 
     virtual ~SysInfo();
 
     virtual void init() = 0;
     virtual double cpuLoadAverage() = 0;
     virtual double memoryUsed() = 0;
+
+protected:
+
+    explicit SysInfo();                     //Singleton: Nur die Klasse selbst und die Kinder dürfen den Konstruktor aufrufen
+
+private:
+
+    SysInfo(const SysInfo& rhs);            //Singleton: Copy-Konstruktor private machen
+    SysInfo& operator=(const SysInfo& rhs); //Singleton: Zuweisungs-Operator private machen
 
 };
 
